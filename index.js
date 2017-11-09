034f05f93f1601e460f87233af9b30beaed1831b
Tony.Liang <pillar0514@163.com>
2017-11-09 17:00:12 +0800

    Show upload final state in console.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const $gutil = require('gulp-util');
 const $through = require('through2');
 const $qcloudUpload = require('qcloud-cos-upload');
 
+const $colors = $gutil.colors;
+
 const PLUGIN_NAME = 'gulp-qcloud-cos-upload';
 
 const isFile = path => $fs.existsSync(path) && $fs.statSync(path).isFile();
@@ -27,6 +29,11 @@ const upload = options => {
 		log: true,
 		overwrite: false
 	}, requestParam, options);
+
+	let nTotal = 0;
+	let nSkip = 0;
+	let nFailed = 0;
+	let nSuccess = 0;
 
 	return $through.obj(function (file, encoding, callback) {
 		let hasAllRequestParam = Object.keys(requestParam).every(key => {
@@ -61,6 +68,7 @@ const upload = options => {
 			let uploadPath = $path.join(conf.prefix, relativePath);
 			uploadPath = uploadPath.replace(/\\/g, '/');
 
+			nTotal++;
 			$qcloudUpload({
 				debug: conf.debug,
 				log: conf.log,
@@ -72,10 +80,32 @@ const upload = options => {
 				Region: conf.Region,
 				FilePath: file.path,
 				Key: uploadPath
-			}).then(() => callback());
+			}).then(rs => {
+				if (rs) {
+					if (rs.isExists) {
+						nSkip++;
+					} else {
+						nSuccess++;
+					}
+				} else {
+					nFailed++;
+				}
+				callback();
+			}).catch(() => {
+				nFailed++;
+				callback();
+			});
 		} else {
 			callback();
 		}
+	}, callback => {
+		$gutil.log(
+			'Total:', $colors.green(nTotal),
+			'Skip:', $colors.gray(nSkip),
+			'Success:', $colors.green(nSuccess),
+			'Failed:', $colors.red(nFailed)
+		);
+		callback(null);
 	});
 };
 
